1126d0845c9b7befe7dc78655d6520c99dd30927
Michal Shelenberg <email@michalshelenberg.com>
2024-06-06 23:47:11 +0200

    PWA installable

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,42 @@
 import Navigation from "@/components/navigation";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "InFaktura";
+const APP_DESCRIPTION = "Faktura online bez registrace a zcela zdarma";
+
 export const metadata: Metadata = {
-  title: "InFaktura",
-  description: "Faktura online bez registrace a zcela zdarma",
+  applicationName: APP_NAME,
+  title: APP_NAME,
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_NAME,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: APP_NAME,
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: APP_NAME,
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#ffffff",
 };
 
 export default function RootLayout({
