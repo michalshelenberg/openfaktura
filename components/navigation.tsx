833f894cb9d21943314db1f8f10b162c0a1dd694
Michal Shelenberg <michalshelenberg@outlook.com>
2024-09-24 08:21:05 +0200

    Add custom font

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,10 +1,15 @@
 import AppsIcon from "@mui/icons-material/Apps";
 import Link from "next/link";
+import localFont from "next/font/local";
+
+const myFont = localFont({
+  src: "../public/fonts/FellixTRIAL-Bold.CkQlcgMT.woff2",
+});
 
 export default function Navigation() {
   return (
-    <div className="flex flex-row gap-2 bg-blue-600 min-h-[50px] h-[50px] z-10 text-white sticky top-0">
-      <nav>
+    <div className="flex flex-row gap-2 min-h-[50px] h-[50px] z-10 sticky top-0 bg-[#003dff] text-white">
+      <nav className="">
         <Link
           href={"#"}
           className="h-full w-[50px] flex items-center justify-center"
@@ -13,7 +18,7 @@ export default function Navigation() {
         </Link>
       </nav>
       <div className="flex flex-row items-center justify-center">
-        <h1 className="font-bold">OpenFaktura</h1>
+        <h1 className={`${myFont.className} font-bold text-xl`}>OpenFaktura</h1>
       </div>
     </div>
   );
